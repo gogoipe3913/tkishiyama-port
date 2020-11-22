257020fc1f9927cf01f66c8c8e9dc65888655e23
gogoipe3913 <taiki.kishiyama0913@gmail.com>
2020-11-22 20:05:25 +0900

    ギャラリーページの追加

---
+++ pages/gallery/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="album"></div>
+</template>
+
+<script>
+export default {
+  name: 'Album'
+};
+</script>
+
+<style scoped lang="scss"></style>
---
+++ pages/gallery/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="gallery"></div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery'
+};
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/sass/index.scss';
+* {
+  color: #444;
+}
+</style>
