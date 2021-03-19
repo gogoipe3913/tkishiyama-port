de964e89151321c89594dee2b9243fa68a426562
gogoipe3913 <taiki.kishiyama0913@gmail.com>
2021-03-20 00:41:26 +0900

    [SP] navのスタイルを修正

--- components/sp/organisms/NavRouter/component.vue
+++ components/sp/organisms/NavRouter/component.vue
@@ -2,22 +2,22 @@
   <div class="NavRouter">
     <div class="NavRouter__secondBackground">
       <ul class="NavRouter__items">
-        <li class="NavRouter__item NavRouter__item--about">
+        <li class="NavRouter__item NavRouter__itemAbout">
           <nuxt-link class="NavRouter__itemButton" :to="topLink">
             <span class="NavRouter__itemButtonText">top</span>
           </nuxt-link>
         </li>
-        <li class="NavRouter__item NavRouter__item--works">
+        <li class="NavRouter__item NavRouter__itemWorks">
           <nuxt-link class="NavRouter__itemButton" :to="workLink">
             <span class="NavRouter__itemButtonText">galleries</span>
           </nuxt-link>
         </li>
-        <li class="NavRouter__item NavRouter__item--blog">
+        <li class="NavRouter__item NavRouter__itemBlog">
           <nuxt-link class="NavigationBar__itemButton" :to="documentLink">
             <span class="NavRouter__itemButtonText">documents</span>
           </nuxt-link>
         </li>
-        <li class="NavRouter__item NavRouter__item--contact">
+        <li class="NavRouter__item NavRouter__itemContact">
           <nuxt-link
             v-scroll-to="'#contact'"
             class="NavRouter__itemButton"
